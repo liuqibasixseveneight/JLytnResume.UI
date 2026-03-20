b947e7d7127152c14fb58494c60e4f2b0c572b7a
liuqiba <liuqiba.sixseveneight@gmail.com>
2026-03-20 15:28:12 +0000

    feat: Improving responsiveness of projects' divider

--- src/components/ui/atoms/SectionDivider/SectionDivider.styles.ts
+++ src/components/ui/atoms/SectionDivider/SectionDivider.styles.ts
@@ -1,13 +1,17 @@
 import styled from 'styled-components';
 
-export const SectionDividerWrapper = styled.div``;
+export const SectionDividerWrapper = styled.div`
+    overflow: hidden;
+    width: 100%;
+`;
 
 export const PlusSign = styled.div`
-  align-items: center;
-  display: flex;
-  font-size: 8rem;
-  justify-content: center;
-  width: 100%;
+    align-items: center;
+    display: flex;
+    font-size: clamp(2rem, 8vw, 8rem);
+    justify-content: center;
+    line-height: 1;
+    width: 100%;
 `;
 
 
